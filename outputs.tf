0edf3d934801eaf06d1b5a5d5b7fe65fd4b0dae0
KobeMadeTech <139927225+KobeCyber@users.noreply.github.com>
2025-10-17 09:46:09 -0500

    Fixing outputs.tf(using ai)

--- outputs.tf
+++ outputs.tf
@@ -1,31 +1,36 @@
 #Work in progress
 
-output "instance_public_ip" {
-  description = "The public IP address of the EC2 instance"
-  value       = aws_instance.example.public_ip
+resource "aws_instance" "example" {
+  count = length(data.aws_subnets.default_subnets.ids)
+  ...
 }
 
-output "instance_private_ip" {
-  description = "The private IP address of the EC2 instance"
-  value       = aws_instance.example.private_ip
+output "instance_public_ips" {
+  description = "Public IPs of the EC2 instances"
+  value       = [for inst in aws_instance.example : inst.public_ip]
 }
 
-output "instance_id" {
-  description = "The ID of the EC2 instance"
-  value       = aws_instance.example.id
+output "instance_private_ips" {
+  description = "Private IPs of the EC2 instances"
+  value       = [for inst in aws_instance.example : inst.private_ip]
+}
+
+output "instance_ids" {
+  description = "IDs of the EC2 instances"
+  value       = [for inst in aws_instance.example : inst.id]
 }
 
 output "instance_ami" {
-  description = "The AMI ID used for the EC2 instance"
-  value       = aws_instance.example.ami
+  description = "AMI ID used by the EC2 instances"
+  value       = var.ami
 }
 
 output "security_group_id" {
-  description = "The ID of the security group attached to the EC2 instance"
+  description = "ID of the security group"
   value       = aws_security_group.example.id
 }
 
 output "vpc_id" {
-  description = "The ID of the VPC where the EC2 instance is deployed"
+  description = "ID of the VPC"
   value       = aws_vpc.example.id
 }
